95b4dc3cc3bef9bf6205c1a412d520ebc8b15576
mscott01 <michael.scott@webtechwireless.com>
2016-07-26 11:20:08 -0700

    Created DemoApp

---
+++ MySolution/DemoApp/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace DemoApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hellow World!");
+            Thread.Sleep(1500);
+        }
+    }
+}
