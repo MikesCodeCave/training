800c1ca9e5da9f30d8171e3ff42da858ef12900d
mscott01 <michael.scott@webtechwireless.com>
2016-07-26 13:57:04 -0700

    Implemented worker

---
+++ MySolution/DemoApp/WorkerImpl.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DemoApp
+{
+    class WorkerImpl : IWorker
+    {
+        public int StartWork()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void StopWork()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
